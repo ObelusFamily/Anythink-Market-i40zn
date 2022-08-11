6fa80c7567b4bc00327b1cbb6d8a8662e359dca1
Kaspar Ustav <54994820+Piiritaja@users.noreply.github.com>
2022-08-12 00:57:02 +0300

    Control on backend

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -18,7 +18,7 @@ const mapDispatchToProps = (dispatch) => ({
 });
 
 const ItemPreview = (props) => {
-  let item = props.item;
+  const item = props.item;
   const handleClick = (ev) => {
     ev.preventDefault();
     if (item.favorited) {
@@ -35,7 +35,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image !== "" ? item.image: "placeholder.png"}
+        src={item.image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
